51e3b82e50966fff1e6d6b8eada790241d63a4cd
Ax-cd <axcd.coder@gmail.com>
2022-04-04 12:29:57 +0200

    add base html

--- src/DrumMachine.jsx
+++ src/DrumMachine.jsx
@@ -1,11 +1,53 @@
 import React, { Component } from "react";
 
 export default class DrumMachine extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
-    return <h1>Drum Machine</h1>;
+    return (
+      <main id="drum-machine">
+        <h1>Drum Machine</h1>
+        <section id="display">
+          <div id="" className="drum-pad">
+            <span>Q</span>
+            <audio src="" id="" className="clip"></audio>
+          </div>
+          <div id="" className="drum-pad">
+            <span>W</span>
+            <audio src="" id="" className="clip"></audio>
+          </div>
+          <div id="" className="drum-pad">
+            <span>E</span>
+            <audio src="" id="" className="clip"></audio>
+          </div>
+          <div id="" className="drum-pad">
+            <span>A</span>
+            <audio src="" id="" className="clip"></audio>
+          </div>
+          <div id="" className="drum-pad">
+            <span>S</span>
+            <audio src="" id="" className="clip"></audio>
+          </div>
+          <div id="" className="drum-pad">
+            <span>D</span>
+            <audio src="" id="" className="clip"></audio>
+          </div>
+          <div id="" className="drum-pad">
+            <span>Z</span>
+            <audio src="" id="" className="clip"></audio>
+          </div>
+          <div id="" className="drum-pad">
+            <span>X</span>
+            <audio src="" id="" className="clip"></audio>
+          </div>
+          <div id="" className="drum-pad">
+            <span>C</span>
+            <audio src="" id="" className="clip"></audio>
+          </div>
+        </section>
+      </main>
+    );
   }
 }
