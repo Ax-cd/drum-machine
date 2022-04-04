3a759c19663a2ae7e77f7ac3653162a8c2c23d1e
Ax-cd <axcd.coder@gmail.com>
2022-04-04 12:11:31 +0200

    update rendering to reactDOMClient - React 18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import DrumMachine from "./DrumMachine";
 
-ReactDOM.render(<DrumMachine />, document.getElementById("root"));
+ReactDOMClient.createRoot(document.getElementById("root")).render(
+  <DrumMachine />
+);
